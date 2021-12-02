90386f93096c350c8c5791badb389d399d4a4851
harun <meteharun@sabanciuniv.edu>
2021-12-02 21:29:44 +0300

    firebase core done

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,10 @@ import 'routes/Walkthrough.dart';
 import 'routes/welcome.dart';
 import 'routes/login.dart';
 import 'routes/signup.dart';
+import "package:firebase_core/firebase_core.dart";
 
-void main() => runApp(MaterialApp(
+
+/*void main() => runApp(MaterialApp(
   initialRoute: "/",
   routes: {
     "/": (context) => Loading(routeName: "/walkthrough"),
@@ -14,4 +16,85 @@ void main() => runApp(MaterialApp(
     "/login": (context) => Login(),
     "/signup": (context) => Signup(),
   },
-));
+));*/
+
+void main(){
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyFirebaseApp());
+}
+
+
+class MyFirebaseApp extends StatefulWidget {
+  const MyFirebaseApp({Key? key}) : super(key: key);
+
+  @override
+  _MyFirebaseAppState createState() => _MyFirebaseAppState();
+}
+
+class _MyFirebaseAppState extends State<MyFirebaseApp> {
+
+  final Future <FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: _initialization,
+        builder: (context,snapshot){
+          if(snapshot.hasError){
+            return MaterialApp(
+              home: Scaffold(
+                body: Center(
+                  child: Text("No Firebase Connection!"),
+                )
+              )
+            );
+          }
+
+          if(snapshot.connectionState == ConnectionState.done){
+            runApp(MaterialApp(
+              initialRoute: "/",
+              routes: {
+                "/": (context) => Loading(routeName: "/walkthrough"),
+                "/walkthrough": (context) => Walkthrough(),
+                "/welcome": (context) => Welcome(),
+                "/login": (context) => Login(),
+                "/signup": (context) => Signup(),
+              },
+            ));
+          }
+
+          return MaterialApp(
+            home: Center(
+              child: Text("Connecting to Firebase",
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.white,
+                backgroundColor: Colors.red,
+              ),),
+            ),
+          );
+
+
+        },);
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("FIREBASEEEEE"),
+      ),
+      body: Center(
+        child: Text("Connected",
+          style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30),),
+      ),
+    );
+  }
+}
+
