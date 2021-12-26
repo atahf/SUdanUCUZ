b38b5087a519f5db7c837d7fdb99f5daf7987204
edlcn <emirdlcn@gmail.com>
2021-12-26 16:59:14 +0300

    import edildi

--- lib/routes/feedView.dart
+++ lib/routes/feedView.dart
@@ -2,6 +2,7 @@ import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:project/models/searchView.dart';
 import 'package:project/routes/addListing.dart';
 import 'package:provider/provider.dart';
 import '../services/auth.dart';
